777a393b2e768cb80d00b023ad80e57c5fe81162
wdelcol12 <wdelcol@gmail.com>
2024-02-26 17:23:17 -0500

    inclass work

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -45,6 +45,9 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify(incidents.filter((incident) => {
                 return incident.id === `MABOS00${id}`
             })));
+        } else {
+            res.writeHead(400, {'Content-Type': 'text/plain'});
+            res.end("Parameters needed in the URL")
         }
     } else {
         // If the request is for an unsupported endpoint, send a not found response
